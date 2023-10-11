5b739029a854d5bdc3315d561b8927e344c59600
Tomm9000 <90690902+Tomm9000@users.noreply.github.com>
2023-10-11 12:02:50 +0200

    Minor Bugfix
    Archer card didnt have proper colliders

--- Assets/Resources/Scripts/Spawner.cs
+++ Assets/Resources/Scripts/Spawner.cs
@@ -9,6 +9,9 @@ public class Spawner : MonoBehaviour
     public int RandomEnemyPrefab;
     public void ButtonPressedCavalry()
     {
+        
+        
+        
         RandomEnemyPrefab = Random.Range(0, EnemyPrefabs.Length);
         GameObject enemyPrefab = Instantiate(EnemyPrefabs[RandomEnemyPrefab], SpawnerLoc.transform.position, SpawnerLoc.transform.rotation);
     }
